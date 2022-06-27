16fd24ba566c4e47b11e69d136f1fab9e6bcf470
Jafet <jafetpintle@gmail.com>
2022-06-27 17:45:53 -0500

    Culminacion del reto sobre list comprehension

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -3,6 +3,9 @@ List comprehensions es una contrucción sintátca para crear una lista
 basada en listas existentes.
 """
 
+from random import randrange
+
+
 def run():
     #Simple form to make a list of squares
     # squares = []
@@ -15,10 +18,16 @@ def run():
     #STRUCTURE[ element = Cada elemento que se agregará a la lista
     #           for element in iterable = Ciclo del cual se extraeran elementos de otra lista o element iterable
     #           if condition] = Condicion para filtrar los elementos del ciclo
-    print(squares)
+    #print(squares)
 
-    
-    
+    """
+    Challenge: Using list comprehension, make a list of all multiplies of 4 that are also multiples of
+    6 and 9 up to 5 digits
+    """
+
+    #Using module equals to 36 because 36 es the least common multiple
+    square_challenge = [i for i in range(1,100000) if  i%36== 0]
+    print(square_challenge)   
     
 
 if __name__ == "__main__":
