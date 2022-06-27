1d83438420fa8d0f831897038e54f270662ff309
Jafet <jafetpintle@gmail.com>
2022-06-27 18:14:30 -0500

    Creacion de diccionario normal

---
+++ dictionary_comprehensions.py
@@ -0,0 +1,13 @@
+def run():
+    #Saving only numbers that no are divisibles by 3 in a dictionary
+    dict = {}
+    for i in range(1,101):
+        if(i%3 !=0):
+            dict.update({i : i**3}) #Other way is dict[i] = i***3
+    print(dict)
+
+
+
+
+if __name__ == "__main__":
+    run()
