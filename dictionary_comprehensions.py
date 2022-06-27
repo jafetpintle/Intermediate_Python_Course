c06fc9b3f4bc8eb89da69d18a076f6155669b163
Jafet <jafetpintle@gmail.com>
2022-06-27 18:25:55 -0500

    Creacion de primer dictionary comprehension

--- dictionary_comprehensions.py
+++ dictionary_comprehensions.py
@@ -1,10 +1,15 @@
 def run():
     #Saving only numbers that no are divisibles by 3 in a dictionary
-    dict = {}
-    for i in range(1,101):
-        if(i%3 !=0):
-            dict.update({i : i**3}) #Other way is dict[i] = i***3
+    # dict = {}
+    # for i in range(1,101):
+    #     if(i%3 !=0):
+    #         dict.update({i : i**3}) #Other way is dict[i] = i***3
+
+    #Using list comprehension
+    dict = {i: i**3 for i in range(1,101) if (i%3 != 0)}
     print(dict)
+
+    
 
 
 
