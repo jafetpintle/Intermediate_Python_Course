a9d2b0a1f09e077f6ef43536c2e6d2f7a1666e5c
Jafet <jafetpintle@gmail.com>
2022-06-28 23:48:13 -0500

    Introduccion de funcionas lambda

---
+++ lambda.py
@@ -0,0 +1,15 @@
+
+"""
+En Python, una funcion lambda es una funcion anonima que carece de nombre.
+"""
+import string
+
+
+def run():
+    #Palindrome is the variable, this use a string and compare if it is a palindrome
+    palindrome = lambda string: string == string[::-1]
+    print(palindrome('ana'))
+
+
+if __name__ == "__main__":
+    run()
